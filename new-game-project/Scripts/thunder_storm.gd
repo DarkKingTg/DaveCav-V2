af693e1a18bf1ec4cc1f016855a37dd25f7cbe11
WaveWalker <suriyakssuriya@gmail.com>
2026-07-13 18:46:46 +0530

    Added Ambience, Rain and Thunder Sound Effects.

---
+++ new-game-project/Scripts/thunder_storm.gd
@@ -0,0 +1,51 @@
+extends Node2D
+
+@onready var flash = $Lightning/Flash
+@onready var thunder1 = $ThunderSoundEffect1
+@onready var thunder2 = $ThunderSoundEffect2
+@onready var rain = $RainSoundEffect
+
+func _ready():
+
+	randomize()
+
+	rain.play()
+
+	start_storm()
+
+
+func start_storm():
+
+	while true:
+
+		await get_tree().create_timer(
+			randf_range(5.0,12.0)
+		).timeout
+
+		await lightning_sequence()
+
+
+func lightning_sequence():
+
+	# First flash
+	flash.color.a = 0.8
+	await get_tree().create_timer(0.05).timeout
+
+	flash.color.a = 0.0
+	await get_tree().create_timer(0.08).timeout
+
+	# Second flash
+	flash.color.a = 1.0
+	await get_tree().create_timer(0.06).timeout
+
+	flash.color.a = 0.0
+
+	# Thunder delay
+	await get_tree().create_timer(
+		randf_range(0.4,1.2)
+	).timeout
+
+	if randi() % 2 == 0:
+		thunder1.play()
+	else:
+		thunder2.play()
